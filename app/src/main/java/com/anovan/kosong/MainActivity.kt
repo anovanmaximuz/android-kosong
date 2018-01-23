a4f785f8dc000c5328fa27fc3395ec7a55076374
ano popfren <ano.popfren@gmail.com>
2018-01-23 11:22:25 +0700

    updatye

--- app/src/main/java/com/anovan/kosong/MainActivity.kt
+++ app/src/main/java/com/anovan/kosong/MainActivity.kt
@@ -2,11 +2,23 @@ package com.anovan.kosong
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.ads.AdSize
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mAdView : AdView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this, "ca-app-pub-6793022751700770~6253670949")
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+
     }
 }
